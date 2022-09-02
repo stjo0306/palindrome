e43a2a48aef45d3c3776335576ffbb1f4403757a
John Steckelberg <jsteckelberg@nteglobal.com>
2022-09-01 21:18:42 -0500

    make program command line driven

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
+const input = process.argv[2];
 const { myIsPalindrome } = require('./palindrome');
-const { testInputs } = require('../inputs');
 
-console.log('Using my palindrome detector module\n');
-testInputs.forEach((input) => {
-  console.log(`\t\'${input}\' is ` + (myIsPalindrome(input) ? '' : 'not ') + 'a palendrome');
-});
-
-console.log('\n');
+console.log(`\t\'${input}\' is ` + (myIsPalindrome(input) ? '' : 'not ') + 'a palendrome');
